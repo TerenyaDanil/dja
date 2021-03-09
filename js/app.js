47f3a1ab6e07be4f11990a4d6ca858e93164456c
TerenyaDanil <47912455+TerenyaDanil@users.noreply.github.com>
2021-03-09 23:22:09 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,51 @@
+
+var swiper = new Swiper('.cases__slider', {
+	autoHeight: true,
+	loop: true,
+	spaceBetween: 100,
+	grabCursor: true,
+	simulateTouch: true,
+	centeredSlides: true,
+	slidesPerView: 1,
+	speed: 700,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	breakpoints: {
+		1025: {
+			slidesPerView: 3,
+
+		}
+	},
+})
+
+
+var swiper = new Swiper('.rev__slider', {
+	autoHeight: true,
+	loop: true,
+
+	grabCursor: true,
+	simulateTouch: true,
+	centeredSlides: true,
+	slidesPerView: 1,
+	speed: 600,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	breakpoints: {
+		1025: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+
+		}
+	},
+})
+
